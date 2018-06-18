a92046ce154f8469eedb727911c059b587094a5c
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-06-18 15:27:01 -0500

    add dice-roller exercise solution

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -1,7 +1,9 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,4 +17,24 @@ public class HomeController {
     public String portfolio() {
         return "portfolio";
     }
+
+    @GetMapping("/roll-dice")
+    public String showDiceLinks() {
+
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rolldice(@PathVariable int guess, Model model) {
+
+        int roll = (int) Math.ceil(Math.random() * 6);
+
+        boolean match = roll == guess;
+
+        model.addAttribute("match", match);
+        model.addAttribute("roll", roll);
+        model.addAttribute("guess", guess);
+        return "roll-dice";
+    }
+
 }
