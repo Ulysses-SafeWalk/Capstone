bb91bc6a3d22431cf9b8dbd3b9309808d771d34d
Zach Gulde <zachgulde@gmail.com>
2018-06-19 11:10:21 -0500

    Add another dice rolling example

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+
 @Controller
 public class HomeController {
     @GetMapping("/")
@@ -20,7 +22,6 @@ public class HomeController {
 
     @GetMapping("/roll-dice")
     public String showDiceLinks() {
-
         return "roll-dice";
     }
 
@@ -35,6 +36,23 @@ public class HomeController {
         model.addAttribute("roll", roll);
         model.addAttribute("guess", guess);
         return "roll-dice";
+    }
+
+    @GetMapping("/roll/{n}/d{sides}")
+    public @ResponseBody HashMap roll(@PathVariable int n, @PathVariable int sides) {
+        HashMap<String, Object> m = new HashMap<>();
+        Integer[] rolls = new Integer[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int random = (int) Math.ceil(Math.random() * sides);
+            rolls[i] = random;
+            sum += random;
+        }
+
+        m.put("rolls", rolls);
+        m.put("sum", sum);
+        return m;
     }
 
 }
