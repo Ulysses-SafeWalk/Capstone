e34894bea4553bc1cab056e64d15eab8b01bfc9b
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-29 12:24:26 -0500

    working on security config, adding user authentication visibility

--- src/main/java/com/codeup/safewalk/controllers/HomeController.java
+++ src/main/java/com/codeup/safewalk/controllers/HomeController.java
@@ -15,44 +15,4 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/portfolio")
-    public String portfolio() {
-        return "portfolio";
-    }
-
-    @GetMapping("/roll-dice")
-    public String showDiceLinks() {
-        return "roll-dice";
-    }
-
-    @GetMapping("/roll-dice/{guess}")
-    public String rolldice(@PathVariable int guess, Model model) {
-
-        int roll = (int) Math.ceil(Math.random() * 6);
-
-        boolean match = roll == guess;
-
-        model.addAttribute("match", match);
-        model.addAttribute("roll", roll);
-        model.addAttribute("guess", guess);
-        return "roll-dice";
-    }
-
-    @GetMapping("/roll/{n}/d{sides}")
-    public @ResponseBody HashMap roll(@PathVariable int n, @PathVariable int sides) {
-        HashMap<String, Object> m = new HashMap<>();
-        Integer[] rolls = new Integer[n];
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int random = (int) Math.ceil(Math.random() * sides);
-            rolls[i] = random;
-            sum += random;
-        }
-
-        m.put("rolls", rolls);
-        m.put("sum", sum);
-        return m;
-    }
-
 }
