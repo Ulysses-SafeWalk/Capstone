815aa1dc2fc89161e2e2cb89187f984e5a064dc7
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-01 16:16:20 -0500

    map element added, marking users current location

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// function test(){
+//     console.log("this is working!");
+// }
+//
+// test();
+
+
+function initialize() {
+    var myLatlng = new google.maps.LatLng(29.426709, -98.489604);
+
+    var mapOptions = {
+        zoom: 18,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+
+            var marker = new google.maps.Marker({
+                position: initialLocation,
+                map: map,
+                animation: google.maps.Animation.DROP
+            })
+        });
+    }
+}
+
+initialize();
