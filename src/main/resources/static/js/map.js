eca88b740e7e2992af9af4ea6ec5f7427aa0ebf4
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-01 16:38:36 -0500

    working on creating custom marker

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -10,6 +10,7 @@
 function initialize() {
     var myLatlng = new google.maps.LatLng(29.426709, -98.489604);
 
+
     var mapOptions = {
         zoom: 18,
         center: myLatlng,
@@ -17,6 +18,12 @@ function initialize() {
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+    var codeupMarker = new google.maps.Marker({
+        position: myLatlng,
+        icon: "codeup.png",
+        map: map
+    });
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
