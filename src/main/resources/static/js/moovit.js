9cb60fb81c12220ee2aea8558e044edf3e2afb61
Jennifer Walker <dataqueen16@gmail.com>
2018-07-06 12:27:55 -0500

    created click function for uber button to gather current location coordinates and feed them into uber url

--- src/main/resources/static/js/moovit.js
+++ src/main/resources/static/js/moovit.js
@@ -11,6 +11,24 @@
         window.location = `www.moovitapp.com/tripplan?customerId=<ID>&metroId=${METRO_ID}`;
     });
 
+
+    $('#uber').click(function(event) {
+        let geocoder = new google.maps.Geocoder;
+        let crd = window.SafeWalkGeo.position;
+        let latlng = {lat: parseFloat(crd.latitude), lng: parseFloat(crd.longitude)};
+
+        geocoder.geocode({'location': latlng}), function (results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    let formatted_address = results[0].formatted_address;
+                    window.location = `https://m.uber.com/?action=setPickup&pickup[latitude]=${latitude}&pickup[longitude]=${longitude}&pickup[formatted_address]=${formatted_address}`;
+                } else {
+                    window.alert('Could not access your current location');
+                }
+            }
+        }
+    });
+
     // https://www..moovitapp.com/tripplan?customerId=<ID>&metroId=<METRO_ID>&lang=<LANG_CODE>&to=<DISPLAY ADDRESS>&tll=<LAT_LON>
 })();
 
