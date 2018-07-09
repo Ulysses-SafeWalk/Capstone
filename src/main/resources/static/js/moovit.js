ca684506973214d9b631652ae9793920d538d1fe
Jennifer Walker <dataqueen16@gmail.com>
2018-07-09 09:19:37 -0500

    modified html of home page, uber & lyft logic

--- src/main/resources/static/js/moovit.js
+++ src/main/resources/static/js/moovit.js
@@ -16,12 +16,21 @@
     $('#uber').click(function(event) {
         let geocoder = new google.maps.Geocoder;
         let crd = window.SafeWalkGeo.position;
-        let latlng = {lat: parseFloat(crd.latitude), lng: parseFloat(crd.longitude)};
+        let latitude = parseFloat(crd.latitude);
+        let longitude = parseFloat(crd.longitude);
+        let latlng = {lat: latitude, lng: longitude};
 
-        geocoder.geocode({'location': latlng}), function (results, status) {
+        geocoder.geocode({'location': latlng}, function (results, status) {
+            console.log(status);
+            if (status === 'OK') {
+                if (results[0]) {
                     let currentAddress = results[0].formatted_address;
                     window.location = `https://m.uber.com/?action=setPickup&client_id=oQg4FB9S9bs-0xxQ7VcrlCp0raIrWxTd&pickup[latitude]=${latitude}&pickup[longitude]=${longitude}&pickup[formatted_address]=${currentAddress}&dropoff[formatted_address]=Codeup%2C%20Navarro%20Street%2C%20San%20Antonio%2C%20TX%2C%20USA&dropoff[latitude]=29.426786&dropoff[longitude]=-98.489576`;
+                }
+            } else {
+                window.alert('Unable to map your current location');
             }
+        });
     });
 
 
