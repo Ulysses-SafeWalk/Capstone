6de6095d2340c3397cb17386152878b1872e36d8
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-07-16 13:06:43 -0500

    fixed the twilio bug

--- src/main/resources/static/js/TwilioFrontend.js
+++ src/main/resources/static/js/TwilioFrontend.js
@@ -1,4 +1,13 @@
 
+function sendIt(latitude, longitude, buttonType){
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("latitude=" + latitude + "&longitude=" + longitude + "&buttonType=" + buttonType);
+    console.log('xhr request process');
+}
+
 $('#emergency').click(function() {
     let geocoder = new google.maps.Geocoder;
     let crd = window.SafeWalkGeo.position;
@@ -6,34 +15,16 @@ $('#emergency').click(function() {
     let longitude = parseFloat(crd.longitude);
     let buttonType = "0";
 
-    console.log(latitude);
-    console.log(longitude);
-    console.log(buttonType);
+    sendIt(latitude, longitude, buttonType);
 
-    // $.post("/",{latitude: latitude,
-    //                 longitude: longitude,
-    //                 buttonType: buttonType
-    //     }, function(){
-    //         console.log("data sent");
-    //     },)});
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/', true);
-    //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send("latitude=" + latitude + "&longitude=" + longitude + "&buttonType=" + buttonType);
-    console.log('xhr request process');
 });
 
 $('#safe').click(function() {
     let latitude = 37.3320;
     let longitude = 77.2736;
     let buttonType = "1";
-    $.post(
-        "/",
-        {latitude: latitude,
-            longitude: longitude,
-            buttonType: buttonType
-        }, function(){
-            console.log("data sent");
-        },)});
+
+    sendIt(latitude, longitude, buttonType);
+});
+
